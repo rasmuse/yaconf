0a39a580e56af831cc9ac1957abea35ddfacc8f0
Rasmus Einarsson <mr@rasmuseinarsson.se>
2019-04-09 22:07:36 +0200

    Pass tests

--- yaconf/__init__.py
+++ yaconf/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Rasmus Einarsson"""
 __email__ = 'mr@rasmuseinarsson.se'
 __version__ = '0.1.0'
 
-from yaconf.yaconf import *
+from yaconf.yaconf import *  # noqa: F401,F403
--- yaconf/yaconf.py
+++ yaconf/yaconf.py
@@ -3,7 +3,6 @@
 import sys
 import os
 import json
-import logging
 from pathlib import Path
 from types import MappingProxyType as frozendict
 from typing import (
@@ -13,15 +12,13 @@ from typing import (
     Any,
     Union,
     Optional,
-    List,
     )
-
-import functools
 
 PathLike = Union[Path, str]
 ConfigMapping = Mapping[Any, Any]
 Decoder = Callable[[str], ConfigMapping]
 Loader = Callable[[], ConfigMapping]
+
 
 class ConfigReader:
     def __init__(self, loaders: Sequence[Loader]):
@@ -62,16 +59,18 @@ class ConfigReader:
 
 _DEFAULT_CONFIG_FILENAME = '.{app_name}conf'
 
+
 def get_default_config_dir(app_name: str) -> str:
     if sys.platform == 'win32':
-        return path.expandvars(rf'%LOCALAPPDATA%\{app_name}')
+        return os.path.expandvars(rf'%LOCALAPPDATA%\{app_name}')
 
     return os.path.expanduser(f'~/.config/{app_name}/')
 
+
 def get_default_paths(
-    app_name: str,
-    filename: Optional[PathLike] = None
-    ) -> Sequence[PathLike]:
+        app_name: str,
+        filename: Optional[PathLike] = None
+        ) -> Sequence[PathLike]:
 
     if filename is None:
         filename = _DEFAULT_CONFIG_FILENAME.format(app_name=app_name)
@@ -79,8 +78,8 @@ def get_default_paths(
     user_config_dir = get_default_config_dir(app_name)
 
     paths = [
-        filename, # first check local directory
-        os.path.join(user_config_dir, filename) # then user config directory
+        filename,  # first check local directory
+        os.path.join(user_config_dir, filename)  # then user config directory
         ]
 
     return paths
@@ -109,15 +108,15 @@ class FileLoader:
     def __repr__(self):
         return f"{type(self).__name__}('{self.path}')"
 
-import typing
 
 def get_file_reader(
-    app_name: str,
-    filename: Optional[PathLike] = None,
-    decoder: Decoder = typing.cast(Decoder, json.loads)
-    ) -> ConfigReader:
+        app_name: str,
+        filename: Optional[PathLike] = None,
+        decoder: Decoder = json.loads
+        ) -> ConfigReader:
 
     paths = get_default_paths(app_name, filename)
+
     loaders = [FileLoader(path, decoder) for path in paths]
 
     return ConfigReader(loaders)
